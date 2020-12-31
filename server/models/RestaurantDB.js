7860c185c8232099072542b47339c5d46308a83a
Bellriox <autijw@gmail.com>
2020-12-31 14:36:54 +0800

    Filter of restaurants by pricing / rating. Better design for name and review number.

--- server/models/RestaurantDB.js
+++ server/models/RestaurantDB.js
@@ -192,6 +192,114 @@ class RestaurantDB {
             }
         });
     }
+
+    getRestaurantsByCategorySortRating(request, response) {
+        var sql = "SELECT * FROM restaurant.restaurant WHERE restaurant_category = ? ORDER BY restaurant_rating DESC"
+        var value = request.params.category
+        db.query(sql, [value], function(error, result) {
+            if (error) {
+                console.log(error)
+            } else {
+                console.log(`${result.length} result found for ${value} category. Sorted according to ratings`)
+
+                var array = []
+
+                for (var i = 0; i < result.length; i++) {
+                    var restaurantId = result[i].restaurant_id
+                    var restaurantName = result[i].restaurant_name
+                    var restaurantDisplayPhoto = result[i].restaurant_displayPhoto
+                    var restaurantCategory = result[i].restaurant_category
+                    var restaurantLink = result[i].restaurant_link
+                    var restaurantAddress = result[i].restaurant_address
+                    var restaurantRegion = result[i].restaurant_region
+                    var restaurantPricing = result[i].restaurant_pricing
+                    var restaurantRating = result[i].restaurant_rating
+                    var restaurantAbout = result[i].restaurant_about
+                    var restaurantPhone = result[i].restaurant_phone
+                    var restaurantOpeningHours = result[i].restaurant_openingHours
+                    var restaurantReviewsCount = result[i].restaurant_reviewsCount
+                    var restaurantStarCount = result[i].restaurant_starCount
+                    var restaurantGallery = result[i].restaurant_gallery
+
+                    array.push({
+                        "restaurantId": restaurantId,
+                        "restaurantName": restaurantName,
+                        "restaurantDisplayPhoto": restaurantDisplayPhoto,
+                        "restaurantCategory": restaurantCategory,
+                        "restaurantLink": restaurantLink,
+                        "restaurantAddress": restaurantAddress,
+                        "restaurantRegion": restaurantRegion,
+                        "restaurantPricing": restaurantPricing,
+                        "restaurantRating": restaurantRating,
+                        "restaurantAbout": restaurantAbout,
+                        "restaurantPhone": restaurantPhone,
+                        "restaurantOpeningHours": restaurantOpeningHours,
+                        "restaurantReviewsCount": restaurantReviewsCount,
+                        "restaurantStarCount": restaurantStarCount,
+                        "restaurantGallery": restaurantGallery
+                    })
+
+                }
+                response.send(array)
+            }
+        });
+    }
+
+    getRestaurantsByCategorySortPricing(request, response) {
+        var sql = `SELECT * FROM restaurant.restaurant WHERE restaurant_category = ?
+        ORDER BY case restaurant_pricing WHEN "$" THEN 1
+        WHEN "$$" THEN 2
+        WHEN "$$$" THEN 3
+        end`
+        var value = request.params.category
+        db.query(sql, [value], function(error, result) {
+            if (error) {
+                console.log(error)
+            } else {
+                console.log(`${result.length} result found for ${value} category. Sorted according to pricing`)
+
+                var array = []
+
+                for (var i = 0; i < result.length; i++) {
+                    var restaurantId = result[i].restaurant_id
+                    var restaurantName = result[i].restaurant_name
+                    var restaurantDisplayPhoto = result[i].restaurant_displayPhoto
+                    var restaurantCategory = result[i].restaurant_category
+                    var restaurantLink = result[i].restaurant_link
+                    var restaurantAddress = result[i].restaurant_address
+                    var restaurantRegion = result[i].restaurant_region
+                    var restaurantPricing = result[i].restaurant_pricing
+                    var restaurantRating = result[i].restaurant_rating
+                    var restaurantAbout = result[i].restaurant_about
+                    var restaurantPhone = result[i].restaurant_phone
+                    var restaurantOpeningHours = result[i].restaurant_openingHours
+                    var restaurantReviewsCount = result[i].restaurant_reviewsCount
+                    var restaurantStarCount = result[i].restaurant_starCount
+                    var restaurantGallery = result[i].restaurant_gallery
+
+                    array.push({
+                        "restaurantId": restaurantId,
+                        "restaurantName": restaurantName,
+                        "restaurantDisplayPhoto": restaurantDisplayPhoto,
+                        "restaurantCategory": restaurantCategory,
+                        "restaurantLink": restaurantLink,
+                        "restaurantAddress": restaurantAddress,
+                        "restaurantRegion": restaurantRegion,
+                        "restaurantPricing": restaurantPricing,
+                        "restaurantRating": restaurantRating,
+                        "restaurantAbout": restaurantAbout,
+                        "restaurantPhone": restaurantPhone,
+                        "restaurantOpeningHours": restaurantOpeningHours,
+                        "restaurantReviewsCount": restaurantReviewsCount,
+                        "restaurantStarCount": restaurantStarCount,
+                        "restaurantGallery": restaurantGallery
+                    })
+
+                }
+                response.send(array)
+            }
+        });
+    }
 }
 
 module.exports = RestaurantDB;
--- server/routes/routeRestaurant.js
+++ server/routes/routeRestaurant.js
@@ -11,6 +11,10 @@ router.get("/id/:id", restaurantDBObject.getRestaurants);
 
 router.get("/category/:category", restaurantDBObject.getRestaurantsByCategory)
 
+router.get("/category/:category/sortRating", restaurantDBObject.getRestaurantsByCategorySortRating)
+
+router.get("/category/:category/sortPricing", restaurantDBObject.getRestaurantsByCategorySortPricing)
+
 router.get("/all", restaurantDBObject.getAllRestaurants)
 
 module.exports = router;
